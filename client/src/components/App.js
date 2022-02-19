349b5f2523243cd02a7a544086d993e25cb7540c
Praveen Kumar <praveenscience@gmail.com>
2022-02-19 12:24:29 +0000

    Added the service call.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    Hello: ""
+  };
+  componentDidMount() {
+    fetch("/api/hello")
+      .then(raw => raw.json())
+      .then(json => {
+        this.setState({ Hello: json });
+      });
+  }
   render() {
     return (
       <div className="App">
-        <p>Hello World</p>
+        <p>{this.state.Hello}</p>
       </div>
     );
   }
